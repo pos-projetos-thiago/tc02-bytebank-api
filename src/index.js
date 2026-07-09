96b65709c6eff94d7bca49428299807020236f3a
Thiago Soares <thibapso@gmail.com>
2026-07-09 00:47:47 -0300

    fix: update mongodb memory server for render

--- src/index.js
+++ src/index.js
@@ -34,14 +34,15 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-const serverPromise = connectDB().then(() => {
+const serverPromise = (async () => {
+    await connectDB();
     if (process.env.NODE_ENV !== 'test') {
         const port = process.env.PORT || 4000;
         app.listen(port, () => {
             console.log(`Servidor rodando na porta ${port}`);
         });
     }
-});
+})();
 
 
 module.exports = app
